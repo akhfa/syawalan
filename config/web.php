e3e4b3325a9ada2d60fb0ab1da0272ce827d33c8
akhfa <akhmadfakhoni@gmail.com>
2015-07-17 05:59:34 +0700

    change language to id

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'id',
     'components' => [
         'reCaptcha' => [
             'name' => 'reCaptcha',
