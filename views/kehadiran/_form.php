cc4f42bb5f22ed71603a7102181f70275892fb07
akhfa <akhmadfakhoni@gmail.com>
2015-07-20 21:32:02 +0700

    form create kehadiran menyesuaikan form pendaftaran

--- views/kehadiran/_form.php
+++ views/kehadiran/_form.php
@@ -12,19 +12,27 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'nama')->textInput(['maxlength' => true, 'placeholder' => 'Nama*'])->label(false) ?>
 
-    <?= $form->field($model, 'alamat')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'alamat')->textarea(['rows' => 6, 'placeholder' => 'Alamat'])->label(false) ?>
 
-    <?= $form->field($model, 'hp')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'hp')->textInput(['maxlength' => true, 'placeholder' => 'Nomor HP*'])->label(false) ?>
 
-    <?= $form->field($model, 'bbm')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'bbm')->textInput(['maxlength' => true, 'placeholder' => 'Pin BBM'])->label(false) ?>
 
-    <?= $form->field($model, 'line')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'line')->textInput(['maxlength' => true, 'placeholder' => 'ID Line'])->label(false) ?>
 
-    <?= $form->field($model, 'whatsapp')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'whatsapp')->textInput(['maxlength' => true, 'placeholder' => 'Nomor Whatsapp'])->label(false) ?>
 
-    <?= $form->field($model, 'angkatan')->textInput(['maxlength' => true]) ?>
+    <?php
+    $array = array();
+    for($i = 1991; $i < 2016 ; $i++)
+    {
+        array_push($array, array($i => $i));
+    }
+    ?>
+
+    <?= $form->field($model, 'angkatan')->dropDownList($array, ['prompt'=>'Angkatan Pensiun Dewan*'])->label(false) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
